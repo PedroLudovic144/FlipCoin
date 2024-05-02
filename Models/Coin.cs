49173cee67b791020c4a6d220cb79e319b9f0492
PedroLudovic144 <pedroludovic5@gmail.com>
2024-05-02 12:36:20 -0300

    New commit

---
+++ Models/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin.Models
+{
+    internal class Coin
+    {
+        public string Lado { get; set; }
+        public int LadoEscolido { get; set; }
+        public Coin()
+        {
+
+        }
+        public  void Jogar()//string
+        {
+            Random aleatorio = new Random();
+            int randomNumber = aleatorio.Next(2);
+
+        }
+
+    }
+}
