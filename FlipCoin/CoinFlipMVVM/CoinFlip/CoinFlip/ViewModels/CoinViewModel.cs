587dcafc3659ba804893bd82551249a7075bba63
PedroLudovic144 <pedroludovic5@gmail.com>
2024-06-13 12:17:36 -0300

    New commit

---
+++ FlipCoin/CoinFlipMVVM/CoinFlip/CoinFlip/ViewModels/CoinViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CoinFlip.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CoinFlip.ViewModels
+{
+    public partial class CoinViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string resultado;
+
+        [ObservableProperty]
+        private string imagem;
+
+        [ObservableProperty]
+        private string escolha;
+
+        public ICommand JogarCommand { get;}
+        
+        public CoinViewModel()
+        {
+            JogarCommand = new Command(Jogar);
+        }
+
+        public void Jogar()
+        {
+            Coin coin = new Coin();
+            coin.Jogar();
+            if (Escolha == coin.LadoSorteado) {
+                Resultado = "Você venceu";
+
+            }
+            else
+            {
+                Resultado = "Você perdeu";
+            }
+        }
+    }
+}
